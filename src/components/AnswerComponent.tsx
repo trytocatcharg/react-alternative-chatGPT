cdd88f66017c3fb63561a79b1b23140b7d1dc8d6
trytocatch <blockchain@DESKTOP-C6LHPJ9.localdomain>
2023-04-17 14:56:58 +0200

    fix styles

--- src/components/AnswerComponent.tsx
+++ src/components/AnswerComponent.tsx
@@ -14,7 +14,7 @@ const AnswerComponent = ({storedValues}: AnswerProps) => {
                         return (
                             <div className="answer-section" key={index}>
                                 <p className="question">{value.question}</p>
-                                <p className="answer">{value.answer}</p>
+                                <p className="answer"  dangerouslySetInnerHTML={{__html:value.answer as string}} ></p>
                                 <div className="copy-icon">
                                     <i className="fa-solid fa-copy"></i>
                                 </div>
